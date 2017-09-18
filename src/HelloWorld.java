d77f055519fe086a42ae6cb2c7c7316b516d910b
VanBiak <s281867@ocsb.local>
2017-09-18 12:57:17 -0400

    First Save

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// This is a single line comment
+		/* This has 
+		 * many lines
+		 * 
+		 */
+		System.out.println("Hello, I'm Van!");
+		System.out.println("This is my first program");
+		System.out.println("Isn't this fun??");
+		System.out.println("He said \"Do this now!\"");
+	}
+
+}
